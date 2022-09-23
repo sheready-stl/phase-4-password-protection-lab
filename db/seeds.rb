5f5ab84fdd0edb609ab6dc4fd20dabb40de81abd
sheready-stl <stella.waithera@moringaschool.com>
2022-09-24 02:16:31 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Abzed", password: "12345678")
+User.create(username: "tez", password: "qwe123")
